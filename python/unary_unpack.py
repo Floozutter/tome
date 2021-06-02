79c688ce8e8effc3ce85532a7a932e5e73536686
Floozutter <me@floozutter.site>
2021-06-01 22:22:00 -0700

    start unary_unpack.py

---
+++ python/unary_unpack.py
@@ -0,0 +1,17 @@
+"""
+getting the value out of a singleton set!
+"""
+
+from typing import TypeVar
+T = TypeVar("T")
+
+def unary_unpack(singleton: set[T]) -> T:
+    value ,= singleton
+    return value
+
+if __name__ == "__main__":
+    singleton = {0xdeadbeef}
+    empty = set()
+    some = {2, 3, 5}
+    many = {range(1_000_000)}
+    print(unary_unpack(singleton))
